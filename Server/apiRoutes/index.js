49b166e9e87a504761bcefe0ce041e4f6e8ba4e0
dfair <dylan-fair@uiowa.edu>
2024-04-28 17:31:47 -0500

    added patient route

--- Server/apiRoutes/index.js
+++ Server/apiRoutes/index.js
@@ -1,11 +1,7 @@
 import express from 'express';
 export const router = express.Router();
 import userRouter from './userRoutes.js'
-// import patientRouter from './patientRoutes.js';
-
-
 
 router.use(userRouter);
-// router.use(patientRouter)
 
 export default router;
